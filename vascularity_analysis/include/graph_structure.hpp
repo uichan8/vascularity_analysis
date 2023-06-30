89a602334ca53c653df589510d71c9f85500f219
uichan <87987154+uichan8@users.noreply.github.com>
2023-06-30 23:17:55 +0900

    add basic structure

---
+++ vascularity_analysis/include/graph_structure.hpp
@@ -0,0 +1,80 @@
+#ifndef _graph_structure
+#define _graph_structure
+
+#include "opencv2/opencv.hpp"
+#include <vector>
+
+//-----------------------------------------------------------------------
+//--------------------------   구조체   ---------------------------------
+//-----------------------------------------------------------------------
+
+class vnode {
+protected:
+	std::vector<vnode*> head, tail;
+
+public:
+	//getter
+	std::vector<vnode*>& get_head();
+	std::vector<vnode*>& get_tail();
+
+	//method
+	void change_head_tail();		//트리 구조에서 노드의 방향성을 바꾸는 함수 head쪽이 optical_disk와 가까운 쪽
+	void push_head(vnode* target);	//head쪽에 대상 노드를 연결
+	void push_tail(vnode* target);	//tail쪽에 대상 노드를 연결 -> graph 클래스에서 두개를 합치는 메소드 생성 해야함
+	void disconnect(vnode* target); //해당 노드가 특정 노드와 연결되어 있을 때 대상 노드와의 연결을 해제하는 메소드
+};
+
+class vbranch : public vnode {
+private:
+	std::vector<int*> branch_segments;
+	std::vector<int*> branch_r;
+
+public:
+	//getter
+	std::vector<int*>& get_branch_segments();
+	std::vector<int*>& get_branch_r();
+
+	//setter
+	void set_branch_segments(const std::vector<int*>& segments);
+	void set_branch_r(const std::vector<int*>& rValues);
+
+	//method
+	void add_segment_data(int* segment, int* r); // branch에서 여러 부분으로 나눈 후에 각각의 데이터를 추가해주는 메소드
+};
+
+class vbifur : public vnode {
+private:
+	cv::Point center_coor;
+	cv::Mat vbifur_mask;
+
+public:
+	//getter
+	cv::Point& get_center_coor();
+	cv::Mat& get_vbifur_mask();
+
+	//setter
+	void set_center_coor(cv::Point& center);
+	void set_vbifur_mask(cv::Mat& mask);
+};
+
+//-----------------------------------------------------------------------
+//--------------------------    구조    ---------------------------------
+//-----------------------------------------------------------------------
+class vgraph {
+private:
+	std::vector <vbranch> vbranchs;
+	std::vector <vbifur> vbifur;
+};
+
+// 나중에 그래프 완성 후 수정 작업
+class vtree {
+private:
+	vnode* root;
+	std::vector <vbranch> vbranchs;
+	std::vector <vbifur> vbifur;
+
+public:
+};
+
+
+#endif
--- vascularity_analysis/main.cpp
+++ vascularity_analysis/main.cpp
@@ -1,18 +1,9 @@
 #include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
 
 int main() {
-    // 검은 창 생성
-    cv::Mat blackImage = cv::Mat::zeros(cv::Size(640, 480), CV_8UC3);
-
-    // 윈도우 생성 및 이미지 표시
-    cv::namedWindow("Black Image", cv::WINDOW_NORMAL);
-    cv::imshow("Black Image", blackImage);
-
-    // 키 입력 대기
-    cv::waitKey(0);
-
-    // 윈도우 닫기
-    cv::destroyAllWindows();
-
+    cout << "123";
     return 0;
 }
