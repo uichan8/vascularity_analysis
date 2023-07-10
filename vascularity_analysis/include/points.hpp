94a7bf1562446936c37e69a13ed406e44f993509
uichan <87987154+uichan8@users.noreply.github.com>
2023-07-10 22:35:07 +0900

    add find bifur codes

--- vascularity_analysis/include/points.hpp
+++ vascularity_analysis/include/points.hpp
@@ -4,33 +4,23 @@
 #include <vector>
 
 #include "opencv2/opencv.hpp"
-#include "points.hpp"
 
 class points {
 private:
 	//bifur
-	void get_X();
-	void get_T();
-	void get_Y();
-	
-	//end
-	void get_E();
-
-public:
-	//bifur
 	std::vector<cv::Mat> X;
 	std::vector<cv::Mat> T;
 	std::vector<cv::Mat> Y;
-
+	
 	//end
 	std::vector<cv::Mat> E;
 	
+public:
 	//method
 	points();
-
-	
+	void find_bifur_points(cv::Mat &skel_mask, cv::Mat& result_point_mask);
+	void find_end_points(cv::Mat& skel_mask, cv::Mat& result_point_mask);
 };
 
-void test_points();
-
 #endif
+
